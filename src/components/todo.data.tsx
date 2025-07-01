482d47915e58365546f6170c42c3fffcf1ebacf5
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-07-01 11:49:05 +0700

    video 23

---
+++ src/components/todo.data.tsx
@@ -0,0 +1,39 @@
+interface IProps {
+    todos: {
+        id: number,
+        title: string;
+        isComplete: boolean;
+    }[];
+    owner: string;
+    age: number;
+    isDeveloper: boolean;
+}
+
+type TProps = {
+    todos: {
+        id: number,
+        title: string;
+        isComplete: boolean
+    }[];
+    owner: string;
+    age: number;
+    isDeveloper: boolean;
+}
+
+const TodoData = (props: TProps) => {
+    const { todos, owner } = props;
+    return (
+        <div>
+            <div>owner = {owner}</div>
+            {todos.map(item => {
+                return (
+                    <div key={item.id}>
+                        <div>{item.title}</div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default TodoData;
--- src/components/todo.list.tsx
+++ src/components/todo.list.tsx
@@ -1,7 +1,35 @@
+import TodoData from "./todo.data";
 
 const TodoList = () => {
+    const todos = [
+        {
+            id: 1,
+            title: "Learn React TypeScript",
+            isComplete: false
+        },
+        {
+            id: 2,
+            title: "Subscribe Youtube HoiDanIT",
+            isComplete: true
+        },
+        {
+            id: 3,
+            title: "Learn English",
+            isComplete: true
+        },
+    ]
+
     return (
-        <div>todo list</div>
+        <div>
+            <div>todo list</div>
+            <br />
+            <TodoData
+                todos={todos}
+                owner={"NghiaHt"}
+                age={22}
+                isDeveloper={true}
+            />
+        </div>
     )
 }
 
